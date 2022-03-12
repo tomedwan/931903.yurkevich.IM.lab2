70580ce6a5c4d637724c3c25c172f0ee0052b887
tomedwan <ilyauiv2000@gmail.com>
2022-03-12 23:06:03 +0600

    f

---
+++ lab2 6.0/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Simulation_lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.edDollars = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btPredict = new System.Windows.Forms.Button();
+            this.edEuro = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edDollars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edEuro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.edDollars);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btPredict);
+            this.panel1.Controls.Add(this.edEuro);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(902, 96);
+            this.panel1.TabIndex = 0;
+            // 
+            // edDollars
+            // 
+            this.edDollars.DecimalPlaces = 2;
+            this.edDollars.Location = new System.Drawing.Point(499, 46);
+            this.edDollars.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.edDollars.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.edDollars.Name = "edDollars";
+            this.edDollars.Size = new System.Drawing.Size(80, 20);
+            this.edDollars.TabIndex = 6;
+            this.edDollars.Value = new decimal(new int[] {
+            11117,
+            0,
+            0,
+            131072});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 46);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Текущий курс Доллара:";
+            // 
+            // btPredict
+            // 
+            this.btPredict.Location = new System.Drawing.Point(781, 49);
+            this.btPredict.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btPredict.Name = "btPredict";
+            this.btPredict.Size = new System.Drawing.Size(90, 29);
+            this.btPredict.TabIndex = 4;
+            this.btPredict.Text = "Старт/Стоп";
+            this.btPredict.UseVisualStyleBackColor = true;
+            this.btPredict.Click += new System.EventHandler(this.btPredict_Click);
+            // 
+            // edEuro
+            // 
+            this.edEuro.DecimalPlaces = 2;
+            this.edEuro.Location = new System.Drawing.Point(136, 43);
+            this.edEuro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.edEuro.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.edEuro.Name = "edEuro";
+            this.edEuro.Size = new System.Drawing.Size(80, 20);
+            this.edEuro.TabIndex = 2;
+            this.edEuro.Value = new decimal(new int[] {
+            12396,
+            0,
+            0,
+            131072});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текущий курс Евро:";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Minimum = 60D;
+            chartArea1.Name = "ChartArea1";
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Minimum = 60D;
+            chartArea2.Name = "ChartArea2";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 96);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "C";
+            series1.Legend = "Legend1";
+            series1.Name = "Курс Евро";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Курс Доллара";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(902, 351);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 447);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edDollars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edEuro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btPredict;
+        private System.Windows.Forms.NumericUpDown edEuro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown edDollars;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ lab2 6.0/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double rateEuro, rateDollars;
+        int days;
+        bool switcher=false;
+
+        const double k = 0.08;
+        Random random = new Random();
+        private void btPredict_Click(object sender, EventArgs e)
+        {
+            if(switcher)
+            {
+                btPredict.Text = "Start";
+                timer1.Stop();
+            }
+            else
+            {
+                rateEuro = (double)edEuro.Value;
+                rateDollars = (double)edDollars.Value;
+                days = (int)chart1.ChartAreas[0].AxisX.Minimum;
+
+
+                chart1.Series[0].Points.Clear();
+                chart1.Series[0].Points.AddXY(0, rateEuro);
+
+                chart1.Series[1].Points.Clear();
+                chart1.Series[1].Points.AddXY(0, rateDollars);
+
+                btPredict.Text = "Stop";
+                timer1.Start();
+            }
+            switcher =! switcher;
+            
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rateEuro = rateEuro * (1 + k * (random.NextDouble() - 0.5));
+            chart1.Series[0].Points.AddXY(days, rateEuro);
+
+            rateDollars = rateDollars * (1 + k * (random.NextDouble() - 0.5));
+            chart1.Series[1].Points.AddXY(days, rateDollars);
+
+        }
+
+    }
+}
